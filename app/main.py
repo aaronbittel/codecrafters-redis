eec3a1d7a588046c342cba25d3f00dbae65c6932
Aaron Bittel <aaron@bittels.de>
2025-10-23 16:39:32 +0200

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
-import socket  # noqa: F401
+import socket
+import threading
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_sock, addr = server_socket.accept()
-    print(f"client with addr {addr} connected.")
 
     while True:
-        client_sock.recv(1024)
-        client_sock.sendall(b"+PONG\r\n")
+        client_sock, addr = server_socket.accept()
+        print(f"client with addr {addr} connected.")
+        threading.Thread(target=handle_connection, args=(client_sock,)).start()
+
+    server_socket.close()
+
+
+def handle_connection(sock: socket.socket) -> None:
+    while True:
+        if not sock.recv(1024):
+            break
+
+        sock.sendall(b"+PONG\r\n")
+    sock.close()
 
 
 if __name__ == "__main__":
